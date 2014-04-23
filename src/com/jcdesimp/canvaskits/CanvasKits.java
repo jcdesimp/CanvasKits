bbd4a1cb4c9ed43b2184208e917327653a24bfd5
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-04-23 02:49:43 -0400

    Config file added.

--- src/com/jcdesimp/canvaskits/CanvasKits.java
+++ src/com/jcdesimp/canvaskits/CanvasKits.java
@@ -1,7 +1,10 @@
 package com.jcdesimp.canvaskits;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Map;
 
 /**
  * File created by jcdesimp on 4/22/14.
@@ -13,6 +16,34 @@ public class CanvasKits extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
+
+
+
+        getLogger().info("Created by Jcdesimp!");
+
+        //// CONFIG FILE MANAGEMENT ///
+
+        Map<String,Object> oldConfig = getConfig().getValues(true);
+        //Generates new config file if not present
+        saveDefaultConfig();
+        //String header = getConfig().options().header();
+        FileConfiguration config = getConfig();
+
+
+        // checks for missing entries and applies new ones
+        for (Map.Entry<String, Object> entry : config.getDefaults().getValues(true).entrySet()) {
+            if(oldConfig.containsKey(entry.getKey())) {
+                config.set(entry.getKey(),oldConfig.get(entry.getKey()));
+            } else {
+                config.set(entry.getKey(), entry.getValue());
+            }
+
+        }
+
+        saveConfig();
+
+        ////////////////////////////////
+
 
     }
 

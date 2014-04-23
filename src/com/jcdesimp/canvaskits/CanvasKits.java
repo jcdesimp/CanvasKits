b46d4d262ccf27a099aef9277c7153cc0b6c7450
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-04-23 00:11:31 -0400

    Initial commit

---
+++ src/com/jcdesimp/canvaskits/CanvasKits.java
@@ -0,0 +1,19 @@
+package com.jcdesimp.canvaskits;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * File created by jcdesimp on 4/22/14.
+ */
+public class CanvasKits extends JavaPlugin {
+
+    private CanvasKits plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+    }
+
+}

910d30dec617e97dbfcda0d226bc72a2b98407c9
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-05-10 04:19:29 -0400

    Fixed file structure issue

--- src/com/jcdesimp/canvaskits/interfaceview/kitview.java
+++ src/com/jcdesimp/canvaskits/interfaceview/kitview.java
@@ -1,7 +1,31 @@
 package com.jcdesimp.canvaskits.interfaceview;
 
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
 /**
  * File created by jcdesimp on 5/9/14.
  */
-public class kitview {
+public class KitView {
+
+    private Player player;
+
+    //Should be divisible by 9
+    private int viewHeight;
+    private ArrayList<ViewButton> buttons;
+
+
+
+    public KitView(Player p) {
+        this.player = p ;
+    }
+
+    public void showUI() {
+
+    }
+
+    public void closeView() {
+
+    }
 }

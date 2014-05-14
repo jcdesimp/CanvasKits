3ffb20a28b0f91f72212782781d7ee13d5a3745a
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-05-14 01:29:57 -0400

    ItemsAction implemented

---
+++ src/com/jcdesimp/canvaskits/kitstruct/kitactions/ItemsAction.java
@@ -0,0 +1,30 @@
+package com.jcdesimp.canvaskits.kitstruct.kitactions;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+
+/**
+ * File created by jcdesimp on 5/14/14.
+ */
+public class ItemsAction implements KitAction {
+
+    private ItemStack item;
+
+    public ItemsAction(ItemStack item) {
+        this.item = item;
+    }
+
+    /**
+     * Give item to a player
+     * @param player
+     */
+    @Override
+    public void call(Player player) {
+        HashMap<Integer,ItemStack>  left = player.getInventory().addItem(item);
+        player.getWorld().dropItem(player.getLocation(), item);
+
+
+    }
+}
